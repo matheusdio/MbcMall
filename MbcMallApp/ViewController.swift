a0faf718b619596597c3e9ee968de52ae8b83f4c
Matheus <dio.nisio@live.com>
2023-07-19 15:09:48 -0300

    Initial Commit

---
+++ MbcMallApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MbcMallApp
+//
+//  Created by Matheus Dionísio on 19/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
